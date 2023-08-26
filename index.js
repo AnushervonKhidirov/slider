36596fd13d28c78c21c95153c752b506296ec81a
AnushervonKhidirov <ankhid98@gmail.com>
2023-08-26 10:04:21 +0500

    thumbs added

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 // params
 const SLIDER_COLUMNS = 3
 const LOOP_ROTATE = true
+const SHOW_THUMBS = false
 
 // elements
+const sliderWrapper = document.querySelector('.slider_wrapper')
 const sliderElem = document.querySelector('.slider')
 const prevBtn = document.querySelector('.nav_btn_prev')
 const nextBtn = document.querySelector('.nav_btn_next')
+const thumbsWrapper = document.createElement('div')
+
+if (SHOW_THUMBS) {
+    thumbsWrapper.classList.add('thumbs')
+    sliderWrapper.appendChild(thumbsWrapper)
+}
 
 // variables
-const slidesList = [1, 2, 3, 4]
+const slidesList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 const slides = []
+const thumbs = []
 const SLIDER_GAP = parseFloat(window.getComputedStyle(sliderElem).gap)
 
 let activeSlidesIndex = Array(SLIDER_COLUMNS)
@@ -43,6 +52,14 @@ function addSlides() {
         sliderElem.appendChild(slide)
 
         slides.push(slide)
+
+        if (SHOW_THUMBS) {
+            const thumb = document.createElement('div')
+            thumb.classList.add('thumb', `thumb-${index}`)
+            thumbsWrapper.appendChild(thumb)
+
+            thumbs.push(thumb)
+        }
     })
 }
 
@@ -57,7 +74,7 @@ function next() {
     activeSlidesIndex = activeSlidesIndex.map(index => checkActive(++index))
 
     sliderElem.style.transform = `translateX(-${slideWidth}px)`
-    prepareSecCards('right')
+    prepareNextSlide('right')
 
     sliderElem.addEventListener('transitionend', () => {
         sliderElem.classList.remove('next_anim', 'move_animation')
@@ -78,7 +95,7 @@ function prev() {
     activeSlidesIndex = activeSlidesIndex.map(index => checkActive(--index))
 
     sliderElem.style.transform = `translateX(${slideWidth}px)`
-    prepareSecCards('left')
+    prepareNextSlide('left')
 
     sliderElem.addEventListener('transitionend', () => {
         sliderElem.classList.remove('prev_anim', 'move_animation')
@@ -93,11 +110,12 @@ function updateSlider(activeSlides) {
         nextBtn.classList.add('disabled')
         prevBtn.classList.add('disabled')
     } else {
-        resetSecCards()
+        resetNextSlide()
     }
 
     activeSlides.forEach((index, order) => {
         if (!slides[index]) return
+        if (SHOW_THUMBS) thumbs[index].classList.add('active')
         slides[index].classList.add('show')
         slides[index].style.order = order + 1
     })
@@ -105,13 +123,15 @@ function updateSlider(activeSlides) {
     isAnimationEnded = true
 }
 
-function prepareSecCards(side) {
+function prepareNextSlide(side) {
     slides[nextSlide].classList.add('show_next')
     slides[nextSlide].style.width = `${slideWidth - SLIDER_GAP}px`
     slides[nextSlide].style[side] = `-${slideWidth}px`
 }
 
-function resetSecCards() {
+function resetNextSlide() {
+    if (SHOW_THUMBS) thumbs[prevSlide].classList.remove('active')
+
     slides[prevSlide].classList.remove('show')
     slides[nextSlide].classList.remove('show', 'show_next')
 
